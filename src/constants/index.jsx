73d9f9f1d20157c627d4caf008b142137dca1129
Rico Torculas <rtorc777@gmail.com>
2024-08-29 01:39:13 -0400

    constants and assets

---
+++ src/constants/index.jsx
@@ -0,0 +1,106 @@
+export const LINKS = [
+    { text: "About Us", targetId: "about" },
+    { text: "Events", targetId: "events" },
+    { text: "E-board", targetId: "eboard" },
+    { text: "Photos", targetId: "photos" },
+    { text: "Contact", targetId: "contact" },
+];
+
+export const ABOUT = {
+    header: "So, what is IEEE?", 
+    content: `IEEE, pronounced “Eye-triple-E”, stands for the Institute of Electrical and Electronics Engineers.\n
+            
+            IEEE, an association dedicated to advancing innovation and technological excellence for the benefit of humanity, 
+            is the world’s largest technical professional society. It is designed to serve professionals involved in all aspects of the electrical, 
+            electronic and computing fields and related areas of science and technology that underlie modern civilization.\n
+            
+            Rowan IEEE's Student Branch is a University club, much like any other. Our branch is apart of IEEE Region 2. 
+            Students of all majors who want a fun, relaxed place to improve themselves professionally and technically are welcome!`
+};
+
+export const EVENTS = [
+    {
+        image: TEMP,
+        title: "General Meetings",
+        description: "General meetings are held bi-weekly on Fridays at 2PM in the Rowan Hall Auditorium. Typically, we will talk about upcoming events followed by a small presentation, seminar, or guest speaker. Pizza and drinks are provided!"
+    },
+    {
+        image: TEMP,
+        title: "Social Activities",
+        description: "It's always nice to have some fun, so we host many different activities avaliable to all of our members free of charge! Some of our previous activities include paintball, TopGolf, trampoline parks, and many others."
+    },
+    {
+        image: TEMP,
+        title: "Technical Workshops",
+        description: "To engage our members, we like to hold workshops to help you expand on your technical knowledge! Some of our previous workshops include Resume building, Git, Verilog, Arduino, PCB design, and many others."
+    }
+]
+
+export const EBOARD = [
+    {
+        image: TEMP,
+        name: "Craig Droke",
+        position: "Chair",
+        contact: "drokec87@students.rowan.edu"
+    },
+    {
+        image: TEMP,
+        name: "Dom Ficarra",
+        position: "Vice Chair",
+        contact: "ficarr85@students.rowan.edu"
+    },
+    {
+        image: TEMP,
+        name: "Matthew Burney",
+        position: "Treasurer",
+        contact: "mcburn92@students.rowan.edu"
+    },
+    {
+        image: TEMP,
+        name: "Chelsea Small",
+        position: "Secretary",
+        contact: "smallc17@students.rowan.edu"
+    },
+    {
+        image: TEMP,
+        name: "Elise Heim",
+        position: "Activities Chair",
+        contact: "heimel27@students.rowan.edu"
+    },
+    {
+        image: TEMP,
+        name: "Aidan Sharpe",
+        position: "Workshop Chair",
+        contact: "sharpe23@students.rowan.edu"
+    },
+    {
+        image: TEMP,
+        name: "Henry Conde",
+        position: "Tournaments Chair",
+        contact: "condeh59@students.rowan.edu"
+    },
+    {
+        image: TEMP,
+        name: "John Schiede",
+        position: "Service Chair",
+        contact: "schied13@students.rowan.edu"
+    },
+    {
+        image: TEMP,
+        name: "Elisabeth Yap",
+        position: "PR Chair",
+        contact: "yapeli32@students.rowan.edu"
+    },
+    {
+        image: TEMP,
+        name: "Rico Torculas",
+        position: "Webmaster",
+        contact: "torcul85@students.rowan.edu"
+    },
+    {
+        image: TEMP,
+        name: "Kevin Hack",
+        position: "Chancellor of ProfHacks",
+        contact: "hackke83@students.rowan.edu"
+    }
+]
